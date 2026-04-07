dabb223fe1add331db6bb49b3387c9f9931f739f
android <android@LABFAE.local>
2026-04-06 21:54:49 -0300

    aula 0604

---
+++ aula0604/App.js
@@ -0,0 +1,63 @@
+import { use, useState } from "react";
+import  AsyncStorage  from '@react-native-async-storage/async-storage';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+
+export default function Cadastro() {
+
+    const [nome, setNome] = useState("")
+    const [idade, setIdade] = useState("")
+    const [curso, setCurso] = useState("")
+
+    const armazenar = (chave, valor) => {
+    AsyncStorage.setItem(chave, valor)
+    }
+
+   const buscarCurso = async(chave) => {
+    const valor = await AsyncStorage.getItem(chave)
+    setCurso(valor)
+  }
+
+    armazenar('01', nome)
+    armazenar('02', idade)
+    armazenar('03', curso)
+
+    buscarCurso('03')
+
+  const alerta = (nome, curso, idade) => {
+    Alert.alert(
+        "Dados cadastrados", 
+        `Nome: ${nome}
+         Idade: ${idade}
+         Curso: ${curso}`
+    );
+  }
+
+  buscarCurso('03')
+
+    return (
+       <View style={styles.container}>
+         <Text>Nome do Aluno: </Text>
+         <TextInput onChangeText={setNome}></TextInput>
+         <Text>Idade do Aluno: </Text>
+         <TextInput onChangeText={setIdade}></TextInput>
+         <Text>Curso do Aluno: </Text>
+         <TextInput onChangeText={setCurso}></TextInput>
+        
+         <Button onPress={alerta}></Button>
+
+
+         <StatusBar style="auto" />
+       </View>
+     );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
